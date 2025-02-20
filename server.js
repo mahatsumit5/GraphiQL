b761c7b061ef83ec4a877459caa4a573945465d9
mahatsumit5 <mahatsumit5@gmail.com>
2025-02-20 16:55:43 +1100

    GraphQL node js initialized

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+const app = express();
+
+app.use("/graphql", graphqlHTTP({ graphiql: true }));
+app.listen(4000, () => {
+  console.log("listening");
+});
